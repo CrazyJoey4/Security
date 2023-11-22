9a977f40db537cee39a2ea884f2a37f9677a6b2b
Teriyaki <103395356+PauThing@users.noreply.github.com>
2023-11-22 17:21:02 +0800

    Food Order SQL Statement

--- order_delete.php
+++ order_delete.php
@@ -4,21 +4,62 @@
 include('connect.php');
 
 if (isset($_GET['id'])) {
-	$delete_id = $_GET['id'];
+	$DELETE_id = $_GET['id'];
 
-	$select = "SELECT Table_ID FROM order_table WHERE ID = '" . $delete_id . "'";
-	$result = mysqli_query($connected, $select);
-	$row = mysqli_fetch_assoc($result);
-	$TABLE = $row['Table_ID'];
+	$query_select = "SELECT Table_ID FROM order_table WHERE ID = ?";
+	$statement_select = mysqli_prepare($connected, $query_select);
+	if ($statement_select) {
+		mysqli_stmt_bind_param($statement_select, "s", $DELETE_id);
+		mysqli_stmt_execute($statement_select);
+	
+		// Bind the result variable
+		mysqli_stmt_bind_result($statement_select, $tableId);
+	
+		// Fetch the result
+		mysqli_stmt_fetch($statement_select);
+	
+		// Close the statement
+		mysqli_stmt_close($statement_select);
 
-
-	$query = "DELETE FROM order_table WHERE ID = '" . $delete_id . "'";
-
-	if (mysqli_query($connected, $query)) {
-		header("location:Order_Edit.php?id=$TABLE");
+		$TABLE = $tableId;
 	} else {
-		$_SESSION['message'] = "<script>alert('Delete failed. Try again.');</script>";
-		header("location:Order_Edit.php?id=$TABLE&st=failure");
+		echo "Error in preparing the statement: " . mysqli_error($connected);
 	}
+
+
+	$query = "DELETE FROM order_table WHERE ID = ?";
+	$statement = mysqli_prepare($connected, $query);
+
+	// Check if the prepared statement is successfully deleted
+	if ($statement) {
+		mysqli_stmt_bind_param($statement, "s", $DELETE_id);
+
+		// Execute the statement
+		if (mysqli_stmt_execute($statement)) {
+			// Check the number of affected rows after the delete operation
+			$rowsAffected = mysqli_stmt_affected_rows($statement);
+
+			if ($rowsAffected > 0) {
+				header("location:Order_Edit.php?id=$TABLE&st=Cancelled");
+			} else {
+				$_SESSION['message'] = "<script>alert('Order cancel failed. Please try again.');</script>";
+				header("location:Order_Edit.php?id=$TABLE&st=failure");
+			}
+		} else {
+			// echo "Error executing the statement: " . mysqli_stmt_error($statement);
+			$_SESSION['message'] = "<script>alert('Order cancel failed. Please try again.');</script>";
+			header("location:Order_Edit.php?st=failure");
+		}
+
+		// Close the prepared statement
+		mysqli_stmt_close($statement);
+	} else {
+		// echo "Error in preparing the statement: " . mysqli_error($connected);
+		$_SESSION['message'] = "<script>alert('Order cancel failed. Please try again.');</script>";
+		header("location:Order_Edit.php?st=failure");
+	}
+} else {
+	$_SESSION['message'] = "<script>alert('Connection failed. Please try again.');</script>";
+	header("location:Order_Edit.php?st=failure");
 }
 ?>
--- order_update.php
+++ order_update.php
@@ -10,21 +10,42 @@ if (isset($_POST['ID'])) {
 	$QUANTITY = $_POST['Food_quantity'];
 	$TABLE = $_POST['Table_ID'];
 
-	$query = "
-			UPDATE `order_table` SET 
-			`Food_quantity` = '$QUANTITY'
-			WHERE `ID` = '$ID'
-			";
+	$query = "UPDATE `order_table` SET 
+			`Food_quantity` = ?
+			WHERE `ID` = ?";
+	$statement = mysqli_prepare($connected, $query);
 
-	if (mysqli_query($connected, $query)) {
-		header("location:Order_Edit.php?id=$TABLE");
-		$_SESSION['message'] = "<script>alert('Updated !');</script>";
+	// Check if the prepared statement is successfully updated
+	if ($statement) {
+		// Check the number of affected rows after the update operation
+		mysqli_stmt_bind_param($statement, "ss", $QUANTITY, $ID);
+
+		// Execute the statement
+		if (mysqli_stmt_execute($statement)) {
+			$rowsAffected = mysqli_stmt_affected_rows($statement);
+
+			if ($rowsAffected > 0) {
+				$_SESSION['message'] = "<script>alert('Order updated !');</script>";
+				header("location:Order_Edit.php?id=$TABLE");
+			} else {
+				$_SESSION['message'] = "<script>alert('Order update failed. Please try again.');</script>";
+				header("location:Order.php?st=failure");
+			}
+		} else {
+			// echo "Error executing the statement: " . mysqli_stmt_error($statement);
+			$_SESSION['message'] = "<script>alert('Order update failed. Please try again.');</script>";
+			header("location:Order.php?st=failure");
+		}
+
+		// Close the prepared statement
+		mysqli_stmt_close($statement);
 	} else {
-		$_SESSION['message'] = "<script>alert('Update failed. Try again.');</script>";
+		// echo "Error in preparing the statement: " . mysqli_error($connected);
+		$_SESSION['message'] = "<script>alert('Order update failed. Please try again.');</script>";
 		header("location:Order.php?st=failure");
 	}
 } else {
-	$_SESSION['message'] = "<script>alert('Connect failed. Try again.');</script>";
+	$_SESSION['message'] = "<script>alert('Connection failed. Please try again.');</script>";
 	header("location:Order.php?st=failure");
 }
 ?>
