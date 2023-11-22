16bf76bbcdd037d4861d4b42e35df8e638850d22
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-11-22 13:18:22 +0800

    update

--- LoginCheck.php
+++ LoginCheck.php
@@ -1,98 +1,42 @@
-<?php
-    include("connect.php");
-    session_start();
+<?PHP
+include("connect.php");
+session_start();
 
-    $object = new Connect();
+$object = new Connect();
 
-    // Check if the form is submitted
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Verify reCAPTCHA
-        $recaptchaSecretKey = '6LdGKBgpAAAAANTTlv9EblGac0vE9TK2LTrSXH2h';
-        $recaptchaResponse = $_POST['g-recaptcha-response'];
+if (isset($_POST["Sign"])) {
+	$USER_id = $_POST['User_ID'];
+	$USER_pwd = $_POST['User_pwd'];
 
-        $recaptchaUrl = 'https://www.google.com/recaptcha/api/siteverify';
-        $recaptchaData = [
-            'secret' => $recaptchaSecretKey,
-            'response' => $recaptchaResponse,
-        ];
+	$query = "SELECT * FROM user_table WHERE User_ID = ?";
+    $statement = mysqli_prepare($connected, $query);
 
-        $options = [
-            'http' => [
-                'header' => 'Content-type: application/x-www-form-urlencoded',
-                'method' => 'POST',
-                'content' => http_build_query($recaptchaData),
-            ],
-        ];
+    if ($statement) {
+        mysqli_stmt_bind_param($statement, "s", $USER_id);
+        mysqli_stmt_execute($statement);
 
-        $context = stream_context_create($options);
-        $recaptchaResult = file_get_contents($recaptchaUrl, false, $context);
-        $recaptchaResult = json_decode($recaptchaResult, true);
+        $result = mysqli_stmt_get_result($statement);
 
-        if ($recaptchaResult['success']) {
-            // reCAPTCHA verification passed
-            $USER_id = $_POST['User_ID'];
-            $USER_pwd = $_POST['User_pwd'];
+        // Check if a row exists for the given User_ID
+        if ($row = mysqli_fetch_assoc($result)) {
+            // Verify the entered password against the hashed password in the database
+            if (password_verify($USER_pwd, $row['User_pwd'])) {
+                $_SESSION['User_ID'] = $row['User_ID'];
+                $DATETIME = $object->get_datetime();
 
-            $query = "SELECT * FROM user_table WHERE User_ID = ?";
-            $statement = mysqli_prepare($connected, $query);
-
-            if ($statement) {
-                mysqli_stmt_bind_param($statement, "s", $USER_id);
-                mysqli_stmt_execute($statement);
-
-                $result = mysqli_stmt_get_result($statement);
-
-                // Check if a row exists for the given User_ID
-                if ($row = mysqli_fetch_assoc($result)) {
-                    // Verify the entered password against the hashed password in the database
-                    if (password_verify($USER_pwd, $row['User_pwd'])) {
-                        $_SESSION['User_ID'] = $row['User_ID'];
-                        $DATETIME = $object->get_datetime();
-
-                        mysqli_query($connected, "INSERT INTO `attendance_table` (`Staff_ID`, `LoginTime`) VALUES ('".$_SESSION['User_ID']."', '$DATETIME');");
-                        header("location:Dashboard.php");
-                    } else {
-                        $_SESSION['message'] = "Login failed. Please check your ID and password.";
-                        header("location:index.php?st=failure");
-                    }
-                } else {
-                    $_SESSION['message'] = "Login failed. User not found.";
-                    header("location:index.php?st=failure");
-                }
-
-                // Close the statement
-                mysqli_stmt_close($statement);
+                mysqli_query($connected, "INSERT INTO `attendance_table` (`Staff_ID`, `LoginTime`) VALUES ('".$_SESSION['User_ID']."', '$DATETIME');");
+                header("location:Dashboard.php");
+            } else {
+                $_SESSION['message'] = "Login failed. Please check your ID and password.";
+                header("location:index.php?st=failure");
             }
         } else {
-            // reCAPTCHA verification failed
-            $_SESSION['message'] = "reCAPTCHA verification failed. Please try again.";
+            $_SESSION['message'] = "Login failed. User not found.";
             header("location:index.php?st=failure");
         }
+
+        // Close the statement
+        mysqli_stmt_close($statement);
     }
+}
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Your Page Title</title>
-        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-    </head>
-
-    <body>
-
-        <form method="post" action="">
-            <div class="recaptcha" id="recaptcha-container" data-sitekey="6LdGKBgpAAAAAMsydD7B6MPDVDHnFF66JP31kovI"></div>
-
-            <!-- Your other form fields go here -->
-            <input type="text" name="User_ID" placeholder="User ID" required>
-            <input type="password" name="User_pwd" placeholder="Password" required>
-
-            <input type="submit" name="Sign" value="Submit">
-        </form>
-
-    </body>
-
-</html>
--- reCaptcha.php
+++ reCaptcha.php
@@ -1,77 +1,194 @@
 <?php
-// Add your PHP and MySQL configuration and initialization code here
-// For example, you might use mysqli_connect to connect to your database
+include("connect.php");
+
+session_start();
+
+$object = new Connect();
+
+if (!$object->SettedUp()) {
+	header("location:" . $object->base_url . "RegisterForm.php");
+	exit();
+}
 
 // Initialize variables
 $isRecaptchaVerified = false;
 
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Verify reCAPTCHA
-    $recaptchaSecretKey = '6LdGKBgpAAAAANTTlv9EblGac0vE9TK2LTrSXH2h';
-    $recaptchaResponse = $_POST['g-recaptcha-response'];
+	// Verify reCAPTCHA
+	$recaptchaSecretKey = '6LdGKBgpAAAAANTTlv9EblGac0vE9TK2LTrSXH2h';
+	$recaptchaResponse = $_POST['g-recaptcha-response'];
 
-    $recaptchaUrl = 'https://www.google.com/recaptcha/api/siteverify';
-    $recaptchaData = [
-        'secret' => $recaptchaSecretKey,
-        'response' => $recaptchaResponse,
-    ];
+	$recaptchaUrl = 'https://www.google.com/recaptcha/api/siteverify';
+	$recaptchaData = [
+		'secret' => $recaptchaSecretKey,
+		'response' => $recaptchaResponse,
+	];
 
-    $options = [
-        'http' => [
-            'header' => 'Content-type: application/x-www-form-urlencoded',
-            'method' => 'POST',
-            'content' => http_build_query($recaptchaData),
-        ],
-    ];
+	$options = [
+		'http' => [
+			'header' => 'Content-type: application/x-www-form-urlencoded',
+			'method' => 'POST',
+			'content' => http_build_query($recaptchaData),
+		],
+	];
 
-    $context = stream_context_create($options);
-    $recaptchaResult = file_get_contents($recaptchaUrl, false, $context);
-    $recaptchaResult = json_decode($recaptchaResult, true);
+	$context = stream_context_create($options);
+	$recaptchaResult = file_get_contents($recaptchaUrl, false, $context);
+	$recaptchaResult = json_decode($recaptchaResult, true);
 
-    if ($recaptchaResult['success']) {
-        // reCAPTCHA verification passed
-        $isRecaptchaVerified = true;
+	if ($recaptchaResult['success']) {
+		// reCAPTCHA verification passed
+		$isRecaptchaVerified = true;
 
-        // Your further processing logic goes here (e.g., database interactions)
-    } else {
-        // reCAPTCHA verification failed
-        echo '<script>alert("reCAPTCHA verification failed. Please try again.");</script>';
-    }
+		// Your further processing logic goes here (e.g., database interactions)
+	} else {
+		// reCAPTCHA verification failed
+		$_SESSION['message'] = 'reCAPTCHA verification failed. Please try again.';
+		header("location:index.php");
+		exit();
+	}
 }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Page Title</title>
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+	<title>RMS - Login</title>
+	<link rel="icon" href="./media/Restaurant-icon.png">
+
+	<style>
+		body {
+			background-color: #B2D2FC;
+			padding-top: 10px;
+			font-size: 20px;
+			height: 100%;
+			font-family: georgia, garamond, serif;
+			font-style: italic;
+		}
+
+		.insertPic {
+			text-align: center;
+		}
+
+		.wrap {
+			text-align: center;
+			margin-left: auto;
+			margin-right: auto;
+			width: 50%;
+			background: snow;
+			padding: 20px;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+		}
+
+		.wrap .InputText {
+			text-align: center;
+			height: 70px;
+			width: 40%;
+			position: relative;
+		}
+
+		.wrap .InputText input {
+			height: 100%;
+			width: 100%;
+			background: snow;
+			border: none;
+			padding-bottom: 0px;
+			border-bottom: 2px solid silver;
+			font-size: 20px;
+			outline: none;
+		}
+
+		.wrap .InputText input:focus~label,
+		.wrap .InputText input:valid~label {
+			transform: translateY(-40px);
+			transition: all 0.3s ease;
+			color: black;
+			outline: none;
+		}
+
+		.wrap .InputText label {
+			font-size: 20px;
+			position: absolute;
+			bottom: 10px;
+			left: 0;
+			color: grey;
+			pointer-events: none;
+		}
+
+		.submit {
+			background-color: white;
+			color: black;
+			font-family: georgia, garamond, serif;
+			font-size: 15px;
+			text-align: center;
+			text-decoration: none;
+			padding: 10px 100px;
+			border: none;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+		}
+
+		.submit:hover {
+			background-color: #4CAF50;
+			color: white;
+			transition: 0.3s;
+		}
+
+		table {
+			text-align: center;
+			background-color: #FFB3B3;
+			border: 1px solid green;
+			color: green;
+			padding: 10px 100px;
+		}
+	</style>
 </head>
 
 <body>
+	<div class="insertPic">
+		<img src="./media/RMSLogo.jpg" width="370">
+	</div>
 
-    <form method="post" action="">
-        <div class="recaptcha" id="recaptcha-container" data-sitekey="6LdGKBgpAAAAAMsydD7B6MPDVDHnFF66JP31kovI"></div>
+	<br>
 
-        <!-- Your other form fields go here -->
 
-        <input type="submit" value="Submit">
-    </form>
+	<div class="wrap">
+		<center>
+			<?php
+			if (isset($_SESSION['message'])) {
+				$message = $_SESSION['message'];
 
-    <!-- Validation Script -->
-    <script>
-        // Validation
-        var isRecaptchaVerified = <?php echo json_encode($isRecaptchaVerified); ?>;
+				echo "<span>";
+				echo "<table><th>" . $message . "</th></table>";
+				echo "</span>";
+			}
 
-        if (!isRecaptchaVerified) {
-            alert('Please complete the reCAPTCHA.');
-            // Handle the case where reCAPTCHA is not verified
-        }
-    </script>
+			unset($_SESSION['message']);
+			?>
+			<form method="post" id="LoginForm" action="LoginCheck.php">
+				<div class="InputText">
+					<input type="text" name="User_ID" id="User_ID" required>
+					<label>ID</label>
+				</div>
 
+				<br><br>
+
+				<div class="InputText">
+					<input type="password" name="User_pwd" id="User_pwd" required>
+					<label>Password</label>
+				</div>
+
+				<br><br>
+
+				<input type="submit" name="Sign" id="Sign" value="Log In" class="submit">
+				<br>
+
+				<!-- Add reCAPTCHA container below the login button -->
+				<div class="g-recaptcha" data-sitekey="6LdGKBgpAAAAAMsydD7B6MPDVDHnFF66JP31kovI"></div>
+			</form>
+		</center>
+	</div>
 </body>
 
 </html>
