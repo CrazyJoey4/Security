e7c24b7ebcc001b9fa1f2bdd221b7275f4f5ccdf
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2023-11-22 12:32:43 +0800

    Create reCaptcha.php

---
+++ reCaptcha.php
@@ -0,0 +1,78 @@
+<?php
+// Add your PHP and MySQL configuration and initialization code here
+// For example, you might use mysqli_connect to connect to your database
+
+// Initialize variables
+$isRecaptchaVerified = false;
+
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Verify reCAPTCHA
+    $recaptchaSecretKey = 'your_recaptcha_secret_key';
+    $recaptchaResponse = $_POST['g-recaptcha-response'];
+
+    $recaptchaUrl = 'https://www.google.com/recaptcha/api/siteverify';
+    $recaptchaData = [
+        'secret' => $recaptchaSecretKey,
+        'response' => $recaptchaResponse,
+    ];
+
+    $options = [
+        'http' => [
+            'header' => 'Content-type: application/x-www-form-urlencoded',
+            'method' => 'POST',
+            'content' => http_build_query($recaptchaData),
+        ],
+    ];
+
+    $context = stream_context_create($options);
+    $recaptchaResult = file_get_contents($recaptchaUrl, false, $context);
+    $recaptchaResult = json_decode($recaptchaResult, true);
+
+    if ($recaptchaResult['success']) {
+        // reCAPTCHA verification passed
+        $isRecaptchaVerified = true;
+
+        // Your further processing logic goes here (e.g., database interactions)
+    } else {
+        // reCAPTCHA verification failed
+        echo '<script>alert("reCAPTCHA verification failed. Please try again.");</script>';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Page Title</title>
+    <!-- Add the reCAPTCHA script -->
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
+
+<body>
+
+    <form method="post" action="">
+        <div class="recaptcha" id="recaptcha-container" data-sitekey="your_recaptcha_site_key"></div>
+
+        <!-- Your other form fields go here -->
+
+        <input type="submit" value="Submit">
+    </form>
+
+    <!-- Validation Script -->
+    <script>
+        // Validation
+        var isRecaptchaVerified = <?php echo json_encode($isRecaptchaVerified); ?>;
+
+        if (!isRecaptchaVerified) {
+            alert('Please complete the reCAPTCHA.');
+            // Handle the case where reCAPTCHA is not verified
+        }
+    </script>
+
+</body>
+
+</html>
