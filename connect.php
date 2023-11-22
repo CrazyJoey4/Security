24f3ae4a3d83b126acee2a6b54e24d91ecdab9c8
CrazyJoey4 <crazyjoeyooi@gmail.com>
2023-11-23 06:58:58 +0800

    check auth

--- connect.php
+++ connect.php
@@ -14,7 +14,7 @@ if (mysqli_connect_errno()) {
 
 class Connect
 {
-	public $base_url = 'http://localhost:8080/Security';
+	public $base_url = 'http://localhost:8080/Security/';
 	public $connect;
 	public $query;
 	public $statement;
